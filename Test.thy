051a1480aeeaf6c77183b1b248234871114a5dd6
Martin Rau <martin.rau@tum.de>
2019-06-11 16:40:48 +0200

    Test time updates

--- Test.thy
+++ Test.thy
@@ -6,6 +6,71 @@ imports
   "Akra_Bazzi.Akra_Bazzi_Approximation"
 begin
 
+section "Time Analysis Merge Sort"
+
+subsection "Time Analysis take and drop"
+
+fun t_take :: "nat \<Rightarrow> 'a list \<Rightarrow> nat" where
+  "t_take _ [] = 0"
+| "t_take n (x#xs) = (
+    case n of
+      0 \<Rightarrow> 0
+    | Suc n \<Rightarrow> 1 + t_take n xs
+  )"
+
+lemma t_take:
+  "t_take n xs = min n (length xs)"
+  apply (induction n xs rule: t_take.induct)
+    apply (auto split: nat.split)
+  done
+
+definition take_cost :: "nat \<Rightarrow> real" where
+  "take_cost n = real n"
+
+lemma take_cost_nonneg[simp]:
+  "0 \<le> take_cost n"
+  unfolding take_cost_def by simp
+
+lemma t_take_conv_take_cost:
+  "real (t_take n xs) \<le> take_cost (length xs)"
+  unfolding take_cost_def by (auto simp add: min_def t_take)
+
+lemma take_cost_big_theta:
+  "take_cost \<in> \<Theta>(\<lambda>n. n::real)"
+  unfolding take_cost_def by simp
+
+fun t_drop :: "nat \<Rightarrow> 'a list \<Rightarrow> nat" where
+  "t_drop _ [] = 0"
+| "t_drop n (x#xs) = (
+    case n of
+      0 \<Rightarrow> 0
+    | Suc n \<Rightarrow> 1 + t_drop n xs
+  )"
+
+lemma t_drop:
+  "t_drop n xs = min n (length xs)"
+  apply (induction n xs rule: t_drop.induct)
+    apply (auto split: nat.split)
+  done
+
+definition drop_cost :: "nat \<Rightarrow> real" where
+  "drop_cost n = real n"
+
+lemma drop_cost_nonneg[simp]:
+  "0 \<le> drop_cost n"
+  unfolding drop_cost_def by simp
+
+lemma t_drop_conv_drop_cost:
+  "real (t_drop n xs) \<le> drop_cost (length xs)"
+  unfolding drop_cost_def by (auto simp add: min_def t_drop)
+
+lemma drop_cost_big_theta:
+  "drop_cost \<in> \<Theta>(\<lambda>n. n::real)"
+  unfolding drop_cost_def by simp
+
+
+subsection "Time Analysis merge"
+
 fun merge :: "('a::linorder) list \<Rightarrow> 'a list \<Rightarrow> 'a list" where
   "merge (x#xs) (y#ys) = (
     if x \<le> y then 
@@ -37,33 +102,23 @@ lemma t_merge:
     apply (auto)
   done
 
-fun t_take :: "nat \<Rightarrow> 'a list \<Rightarrow> nat" where
-  "t_take _ [] = 0"
-| "t_take n (x#xs) = (
-    case n of
-      0 \<Rightarrow> 0
-    | Suc n \<Rightarrow> 1 + t_take n xs
-  )"
+definition merge_cost :: "nat \<Rightarrow> real" where
+  "merge_cost n = real n"
 
-fun t_drop :: "nat \<Rightarrow> 'a list \<Rightarrow> nat" where
-  "t_drop _ [] = 0"
-| "t_drop n (x#xs) = (
-    case n of
-      0 \<Rightarrow> 0
-    | Suc n \<Rightarrow> 1 + t_drop n xs
-  )"
+lemma merge_cost_nonneg[simp]:
+  "0 \<le> merge_cost n"
+  unfolding merge_cost_def by simp
 
-lemma t_take:
-  "t_take n xs = min n (length xs)"
-  apply (induction n xs rule: t_take.induct)
-    apply (auto split: nat.split)
-  done
+lemma t_merge_conv_merge_cost:
+  "real (t_merge xs ys) \<le> merge_cost (length xs + length ys)"
+  unfolding merge_cost_def by (metis of_nat_mono t_merge)
 
-lemma t_drop:
-  "t_drop n xs = min n (length xs)"
-  apply (induction n xs rule: t_drop.induct)
-    apply (auto split: nat.split)
-  done
+lemma merge_cost_big_theta:
+  "merge_cost \<in> \<Theta>(\<lambda>n. n::real)"
+  unfolding merge_cost_def by simp
+
+
+subsection "Time Analysis msort"
 
 fun msort :: "('a::linorder) list \<Rightarrow> 'a list" where
   "msort [] = []"
@@ -87,54 +142,30 @@ fun t_msort :: "('a::linorder) list \<Rightarrow> nat" where
     t_take n xs + t_drop n xs + t_msort l + t_msort r + t_merge l r
   )"
 
-(* *)
-
-function merge_sort_cost :: "(nat \<Rightarrow> real) \<Rightarrow> nat \<Rightarrow> real" where
-  "merge_sort_cost _ 0 = 0"
-| "merge_sort_cost _ 1 = 1"
-| "2 \<le> n \<Longrightarrow> merge_sort_cost t n =
-    merge_sort_cost t (nat \<lfloor>real n / 2\<rfloor>) + merge_sort_cost t (nat \<lceil>real n / 2\<rceil>) + t n"
+function merge_sort_cost :: "nat \<Rightarrow> real" where
+  "merge_sort_cost 0 = 0"
+| "merge_sort_cost 1 = 1"
+| "2 \<le> n \<Longrightarrow> merge_sort_cost n =
+    take_cost n + drop_cost n + merge_sort_cost (nat \<lfloor>real n / 2\<rfloor>) + merge_sort_cost (nat \<lceil>real n / 2\<rceil>) + merge_cost n"
   by force simp_all
 termination by akra_bazzi_termination simp_all
 
+declare t_take.simps t_drop.simps t_merge.simps[simp del]
+
+lemma t_merge_sort_conv_merge_sort_cost:
+  "real (t_msort xs) \<le> merge_sort_cost (length xs)"
+  oops
+
 lemma merge_sort_nonneg[simp]:
-  "(\<And>n. 0 \<le> t n) \<Longrightarrow> 0 \<le> merge_sort_cost t n"
-  apply (induction t n rule: merge_sort_cost.induct)
+  "0 \<le> merge_sort_cost n"
+  apply (induction n rule: merge_sort_cost.induct)
     apply (auto simp del: One_nat_def)
   done
 
-lemma "t \<in> \<Theta>(\<lambda>n. real n) \<Longrightarrow> (\<And>n. 0 \<le> t n) \<Longrightarrow> merge_sort_cost t \<in> \<Theta>(\<lambda>n. real n * ln (real n))"
+lemma msort_cost:
+  "merge_sort_cost \<in> \<Theta>(\<lambda>n. real n * ln (real n))"
   apply (master_theorem)
-    apply auto
+    apply (auto simp add: take_cost_def drop_cost_def merge_cost_def)
   done
-
-definition take_cost :: "nat \<Rightarrow> real" where
-  "take_cost n = real n"
-
-lemma t_take_conv_take_cost:
-  "real (t_take n xs) \<le> take_cost (length xs)"
-  unfolding take_cost_def by (auto simp add: min_def t_take)
-
-lemma take_cost_big_theta:
-  "take_cost \<in> \<Theta>(\<lambda>n. n::real)"
-  unfolding take_cost_def by simp
-
-definition drop_cost :: "nat \<Rightarrow> real" where
-  "drop_cost n = real n"
-
-lemma t_drop_conv_drop_cost:
-  "real (t_drop n xs) \<le> drop_cost (length xs)"
-  unfolding drop_cost_def by (auto simp add: min_def t_drop)
-
-lemma drop_cost_big_theta:
-  "drop_cost \<in> \<Theta>(\<lambda>n. n::real)"
-  unfolding drop_cost_def by simp
-
-definition take_drop_cost :: "nat \<Rightarrow> real" where
-  "take_drop_cost n = take_cost n + drop_cost n"
-
-lemma take_drop_big_theta:
-  "take_drop_cost \<in> \<Theta>(\<lambda>n. n::real)"
-  unfolding take_drop_cost_def take_cost_def drop_cost_def by simp
 
 end
