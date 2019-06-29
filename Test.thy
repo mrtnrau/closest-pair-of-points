1b0de06550feb63bafdd7fa5e2a2435c646595ca
Martin Rau <martin.rau@tum.de>
2019-06-30 01:44:56 +0200

    adapt test the generic adaptation

--- Test.thy
+++ Test.thy
@@ -1,51 +1,21 @@
 theory Test
-  imports "HOL-Analysis.Analysis"
+  imports Closest_Pair
 begin
 
-type_synonym point = "real * real"
-
-definition dist :: "point \<Rightarrow> point \<Rightarrow> real" where
-  "dist p\<^sub>0 p\<^sub>1 = (fst p\<^sub>0 - fst p\<^sub>1)\<^sup>2 + (fst p\<^sub>0 - fst p\<^sub>1)\<^sup>2"
-
-fun find_closest :: "point \<Rightarrow> point list \<Rightarrow> point" where
-  "find_closest _ [] = undefined"
-| "find_closest _ [p] = p"
-| "find_closest p\<^sub>0 (p # ps) = (
-    let c = find_closest p\<^sub>0 ps in
-    if dist p\<^sub>0 p < dist p\<^sub>0 c then
-      p
-    else
-      c
-  )"
-
-fun bf_closest_pair' :: "(point list \<Rightarrow> point list) \<Rightarrow> point list \<Rightarrow> (point * point)" where
-  "bf_closest_pair' _ [] = undefined"
-| "bf_closest_pair' _ [_] = undefined"
-| "bf_closest_pair' _ [p\<^sub>0, p\<^sub>1] = (p\<^sub>0, p\<^sub>1)"
-| "bf_closest_pair' f (p\<^sub>0 # ps) = (
-    let (c\<^sub>0, c\<^sub>1) = bf_closest_pair' f ps in
-    let p\<^sub>1 = find_closest p\<^sub>0 (f ps) in
-    if dist c\<^sub>0 c\<^sub>1 \<le> dist p\<^sub>0 p\<^sub>1 then
-      (c\<^sub>0, c\<^sub>1)
-    else
-      (p\<^sub>0, p\<^sub>1) 
-  )"
-
-
-fun bf_closest_pair_it_rec' :: "(point list \<Rightarrow> point list) \<Rightarrow> (point * point) \<Rightarrow> point list \<Rightarrow> (point * point)" where
-  "bf_closest_pair_it_rec' _ (c\<^sub>0, c\<^sub>1) [] = (c\<^sub>0, c\<^sub>1)"
-| "bf_closest_pair_it_rec' _ (c\<^sub>0, c\<^sub>1) [_] = (c\<^sub>0, c\<^sub>1)"
-| "bf_closest_pair_it_rec' f (c\<^sub>0, c\<^sub>1) (p\<^sub>0 # ps) = (
+fun gen_closest_pair_it_rec :: "(point list \<Rightarrow> point list) \<Rightarrow> (point * point) \<Rightarrow> point list \<Rightarrow> (point * point)" where
+  "gen_closest_pair_it_rec _ (c\<^sub>0, c\<^sub>1) [] = (c\<^sub>0, c\<^sub>1)"
+| "gen_closest_pair_it_rec _ (c\<^sub>0, c\<^sub>1) [_] = (c\<^sub>0, c\<^sub>1)"
+| "gen_closest_pair_it_rec f (c\<^sub>0, c\<^sub>1) (p\<^sub>0 # ps) = (
     let p\<^sub>1 = find_closest p\<^sub>0 (f ps) in
     if dist c\<^sub>0 c\<^sub>1 < dist p\<^sub>0 p\<^sub>1 then
-      bf_closest_pair_it_rec' f (c\<^sub>0, c\<^sub>1) ps
+      gen_closest_pair_it_rec f (c\<^sub>0, c\<^sub>1) ps
     else
-      bf_closest_pair_it_rec' f (p\<^sub>0, p\<^sub>1) ps
+      gen_closest_pair_it_rec f (p\<^sub>0, p\<^sub>1) ps
   )"
 
-fun bf_closest_pair_it' :: "(point list \<Rightarrow> point list) \<Rightarrow> point list \<Rightarrow> (point * point)" where
-  "bf_closest_pair_it' f (p\<^sub>0 # p\<^sub>1 # ps) = bf_closest_pair_it_rec' f (p\<^sub>0, find_closest p\<^sub>0 (f (p\<^sub>1 # ps))) (p\<^sub>1 # ps)"
-| "bf_closest_pair_it' _ _ = undefined"
+fun gen_closest_pair_it :: "(point list \<Rightarrow> point list) \<Rightarrow> point list \<Rightarrow> (point * point)" where
+  "gen_closest_pair_it f (p\<^sub>0 # p\<^sub>1 # ps) = gen_closest_pair_it_rec f (p\<^sub>0, find_closest p\<^sub>0 (f (p\<^sub>1 # ps))) (p\<^sub>1 # ps)"
+| "gen_closest_pair_it _ _ = undefined"
 
 fun fl :: "(point list \<Rightarrow> point list) \<Rightarrow> point * point \<Rightarrow> point list \<Rightarrow> point * point" where
   "fl _ c [] = (fst c, snd c)"
@@ -74,17 +44,17 @@ fun lists :: "'a list \<Rightarrow> 'a list list" where
 | "lists (x#xs) = (x#xs) # lists xs"
 
 
-lemma bf_closest_pair_it_rec'_conv_foldl:
-  "bf_closest_pair_it_rec' f (c\<^sub>0, c\<^sub>1) ps = foldl (fl f) (c\<^sub>0, c\<^sub>1) (lists ps)"
-  apply (induction f "(c\<^sub>0, c\<^sub>1)" ps arbitrary: c\<^sub>0 c\<^sub>1 rule: bf_closest_pair_it_rec'.induct)
+lemma gen_closest_pair_it_rec_conv_foldl:
+  "gen_closest_pair_it_rec f (c\<^sub>0, c\<^sub>1) ps = foldl (fl f) (c\<^sub>0, c\<^sub>1) (lists ps)"
+  apply (induction f "(c\<^sub>0, c\<^sub>1)" ps arbitrary: c\<^sub>0 c\<^sub>1 rule: gen_closest_pair_it_rec.induct)
   apply (auto simp add: Let_def)
   done
 
-lemma bf_closest_pair'_conv_foldr:
+lemma gen_closest_pair_conv_foldr:
   assumes "n = length ps" "2 \<le> n" "\<And>x. f [x] = [x]"
-  shows "bf_closest_pair' f ps = foldr (fr f) (lists ps) (ps!(n-2), ps!(n-1))"
+  shows "gen_closest_pair f ps = foldr (fr f) (lists ps) (ps!(n-2), ps!(n-1))"
   using assms
-  apply (induction f ps arbitrary: n rule: bf_closest_pair'.induct)
+  apply (induction f ps arbitrary: n rule: gen_closest_pair.induct)
   apply (auto simp add: Let_def case_prod_unfold)
   done
 
@@ -226,21 +196,21 @@ proof -
   qed
 qed
 
-lemma bf_closest_pair'_conv_bf_closest_pair_it_rec':
+lemma gen_closest_pair_conv_gen_closest_pair_it_rec:
   assumes "n = length (p#ps)" "2 \<le> n" "p' = find_closest p (f ps)" "\<And>x. f [x] = [x]"
-  shows "bf_closest_pair' f (p#ps) = bf_closest_pair_it_rec' f (p, p') (p#ps)"
+  shows "gen_closest_pair f (p#ps) = gen_closest_pair_it_rec f (p, p') (p#ps)"
 proof -
   let ?ps = "p # ps"
-  have "bf_closest_pair' f ?ps = foldr (fr f) (lists ?ps) (?ps!(n-2), ?ps!(n-1))"
-    using assms bf_closest_pair'_conv_foldr by blast
+  have "gen_closest_pair f ?ps = foldr (fr f) (lists ?ps) (?ps!(n-2), ?ps!(n-1))"
+    using assms gen_closest_pair_conv_foldr by blast
   also have "... = fr f [p, p'] (foldr (fr f) (lists ?ps) (?ps!(n-2), ?ps!(n-1)))"
     using assms foldr_fr_expand by blast
   also have "... = fl f (foldl (fl f) (p, p') (lists ?ps)) [?ps!(n-2), ?ps!(n-1)]"
     using fl_foldl_conv_fr_foldr assms(4) by presburger
   also have "... = foldl (fl f) (p, p') (lists ?ps)"
     using assms foldl_fl_expand by presburger
-  also have "... = bf_closest_pair_it_rec' f (p, p') ?ps"
-    using bf_closest_pair_it_rec'_conv_foldl by simp
+  also have "... = gen_closest_pair_it_rec f (p, p') ?ps"
+    using gen_closest_pair_it_rec_conv_foldl by simp
   finally show ?thesis .
 qed
 
@@ -249,33 +219,27 @@ lemma cases_list012:
   "P [] \<Longrightarrow> (\<And>x. P [x]) \<Longrightarrow> (\<And>x y xs. P (x # y # xs)) \<Longrightarrow> P xs"
   using induct_list012 by metis
 
-lemma bf_closest_pair'_conv_bf_closest_pair_it':
-  "(\<And>x. f [x] = [x]) \<Longrightarrow> bf_closest_pair' f ps = bf_closest_pair_it' f ps"
-  using bf_closest_pair'_conv_bf_closest_pair_it_rec' by (cases rule: cases_list012) auto
+lemma gen_closest_pair_conv_gen_closest_pair_it:
+  "(\<And>x. f [x] = [x]) \<Longrightarrow> gen_closest_pair f ps = gen_closest_pair_it f ps"
+  using gen_closest_pair_conv_gen_closest_pair_it_rec by (cases rule: cases_list012) auto
 
-
-definition bf_closest_pair :: "point list \<Rightarrow> (point * point)" where
-  "bf_closest_pair ps = bf_closest_pair' (\<lambda>ps. ps) ps"
 
 definition bf_closest_pair_it :: "point list \<Rightarrow> (point * point)" where
-  "bf_closest_pair_it ps = bf_closest_pair_it' (\<lambda>ps. ps) ps"
+  "bf_closest_pair_it ps = gen_closest_pair_it (\<lambda>ps. ps) ps"
 
 lemma bf_closest_pair_conv_bf_closest_pair_it[code_unfold]:
   "bf_closest_pair ps = bf_closest_pair_it ps"
   unfolding bf_closest_pair_def bf_closest_pair_it_def
-  using bf_closest_pair'_conv_bf_closest_pair_it' by simp
+  using gen_closest_pair_conv_gen_closest_pair_it by simp
 
-
-definition closest_pair_7 :: "point list \<Rightarrow> (point * point)" where
-  "closest_pair_7 ps = bf_closest_pair' (take 7) ps"
 
 definition closest_pair_7_it :: "point list \<Rightarrow> (point * point)" where
-  "closest_pair_7_it ps = bf_closest_pair_it' (take 7) ps"
+  "closest_pair_7_it ps = gen_closest_pair_it (take 7) ps"
 
 lemma closest_pair_7_conv_closest_pair_7_it[code_unfold]:
   "closest_pair_7 ps = closest_pair_7_it ps"
   unfolding closest_pair_7_def closest_pair_7_it_def
-  using bf_closest_pair'_conv_bf_closest_pair_it' by simp
+  using gen_closest_pair_conv_gen_closest_pair_it by simp
 
 
 subsection "Alternatives"
@@ -283,14 +247,14 @@ subsection "Alternatives"
 (*
 lemma
   assumes "0 < length ps"
-  shows "bf_closest_pair' f (p\<^sub>0 # ps) = bf_closest_pair_it_rec' f (p\<^sub>0, find_closest p\<^sub>0 (f ps)) ps"
+  shows "bf_closest_pair' f (p\<^sub>0 # ps) = gen_closest_pair_it_rec f (p\<^sub>0, find_closest p\<^sub>0 (f ps)) ps"
   using assms
-  apply (induction f "(p\<^sub>0, find_closest p\<^sub>0 ps)" ps arbitrary: p\<^sub>0 rule: bf_closest_pair_it_rec'.induct)
+  apply (induction f "(p\<^sub>0, find_closest p\<^sub>0 ps)" ps arbitrary: p\<^sub>0 rule: gen_closest_pair_it_rec.induct)
   oops
 
 lemma
-  "bf_closest_pair' f (p\<^sub>0 # p\<^sub>1 # ps) = bf_closest_pair_it_rec' f (p\<^sub>0, p\<^sub>1) (p\<^sub>0 # p\<^sub>1 # ps)"
-  apply (induction f "(p\<^sub>0, p\<^sub>1)" ps arbitrary: p\<^sub>0 p\<^sub>1 rule: bf_closest_pair_it_rec'.induct)
+  "bf_closest_pair' f (p\<^sub>0 # p\<^sub>1 # ps) = gen_closest_pair_it_rec f (p\<^sub>0, p\<^sub>1) (p\<^sub>0 # p\<^sub>1 # ps)"
+  apply (induction f "(p\<^sub>0, p\<^sub>1)" ps arbitrary: p\<^sub>0 p\<^sub>1 rule: gen_closest_pair_it_rec.induct)
   oops
 *)
 
