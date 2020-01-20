1ba517f11e61b8e8d8beaa159bf4eff53340fdac
MartinRau <martin.rau@tum.de>
2020-01-20 17:49:45 +0100

    .

--- benchmark/Benchmarks.m
+++ benchmark/Benchmarks.m
@@ -5,8 +5,8 @@ close all
 %% Experimental Data
 n = [ 10000,  20000,  30000,  40000,  50000,  60000,  70000,  80000,  90000,...
      100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000];
-%s = [     21,     43,    72,      98,    121,    160,    175,    203,   237,...
-%         266,    681,  1115,    1472,   1892,   2498,   2859,   3426,  3861];
+s = [     21,     43,    72,      98,    121,    160,    175,    203,   237,...
+         266,    681,  1115,    1472,   1892,   2498,   2859,   3426,  3861];
 j = [     20,     44,    71,      96,    116,    159,    176,    197,   236,...
          263,    663,  1047,    1419,   1896,   2540,   2822,   3430,  3888];
 c = [     45,     99,   158,     226,    281,    348,    419,    472,   557,...
@@ -32,9 +32,9 @@ fprintf('Basic-7/Basic-D : Mean %f WMean %f\n', mean(c./o), wm_c_o)
 %% Non-linear regression
 model = @(beta,x)(beta(1) + beta(2).*x + beta(3).*x.*log2(x));
 
-%s_var0 = [1.0, 1.0, 1.0];
-%s_var = nlinfit(n, s, model, s_var0, 'ErrorModel', 'proportional');
-%s_fitted = model(s_var, n);
+s_var0 = [1.0, 1.0, 1.0];
+s_var = nlinfit(n, s, model, s_var0, 'ErrorModel', 'proportional');
+s_fitted = model(s_var, n);
 
 j_var0 = [1.0, 1.0, 1.0];
 j_var = nlinfit(n, j, model, j_var0, 'ErrorModel', 'proportional');
@@ -54,7 +54,7 @@ i_fitted = model(i_var, n);
 
 %% Constants
 
-%red    = [197/255,  28/255,   8/255];
+red    = [197/255,  28/255,   8/255];
 orange = [218/255, 124/255,  48/255];
 blue   = [ 57/255, 106/255, 177/255];
 green  = [ 62/255, 150/255,  81/255];
@@ -87,3 +87,26 @@ xlabel('Number of Points' , 'FontWeight', 'bold')
 ylabel('Running Time (ms)', 'FontWeight', 'bold')
 
 legend('Basic-2', 'Basic-7', 'Basic-\delta', 'Isabelle', 'Location', 'best')
+
+%% Figure 2
+figure
+hold on
+grid on
+
+scatter(n, s./j, marker_size, orange, 'filled', 'd')
+scatter(n, c./j, marker_size, green , 'filled', 'o')
+scatter(n, o./j, marker_size, blue  , 'filled', 's')
+scatter(n, i./j, marker_size, purple, 'filled', '^')
+
+plot(n, s./j, 'Color', orange, 'LineWidth', line_width, 'LineStyle', '-')
+plot(n, c./j, 'Color', green , 'LineWidth', line_width, 'LineStyle', '-')
+plot(n, o./j, 'Color', blue  , 'LineWidth', line_width, 'LineStyle', '-.')
+plot(n, i./j, 'Color', purple, 'LineWidth', line_width, 'LineStyle', '-.')
+
+set(gca, 'yScale', 'linear', 'yLim', [0.8, 2.5]);
+set(gca, 'xScale', 'log');
+
+xlabel('Number of Points' , 'FontWeight', 'bold')
+ylabel('Time Ratio over Basic-2', 'FontWeight', 'bold')
+
+legend('Basic-\delta (imp.)', 'Basic-7 (imp.)', 'Basic-\delta (func.)', 'Isabelle (func.)', 'Location', 'best')
